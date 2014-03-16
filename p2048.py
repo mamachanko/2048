d62fcc75e8c70bab164875f18def18307da9adb1
Max <max.brauer@deliveryhero.com>
2014-03-16 15:59:55 +0100

    move tests into seperate module and rename to p2048.py

---
+++ p2048.py
@@ -0,0 +1,27 @@
+def move(row):
+    without_zeroes = filter(bool, row)
+    return right_pad(merge(without_zeroes))
+
+
+def merge(row):
+    result = []
+    digit_stack = list(reversed(row))
+    while digit_stack:
+        if len(digit_stack) == 1:
+            result.append(digit_stack.pop())
+            break
+        a = digit_stack.pop()
+        b = digit_stack.pop()
+        if a == b:
+            result.append(a+b)
+        else:
+            result.append(a)
+            digit_stack.append(b)
+    return result
+
+
+def right_pad(input_list, size=4):
+    result = input_list[:]
+    extension = [0,] * (size - len(result))
+    result.extend(extension)
+    return result
--- test_2048.py
+++ test_2048.py
@@ -1,5 +1,8 @@
 from itertools import permutations
 
+import p2048
+from p2048 import merge, right_pad, move
+
 
 def test_single_digit_move():
     assert [2, 0, 0, 0] == move([0, 0, 2, 0])
@@ -53,32 +56,3 @@ def test_right_pad_list_to_defined_length():
 def test_idempotent_right_pad():
     assert [2, 4] == right_pad([2, 4], size=2)
     assert [2, 4, 8] == right_pad([2, 4, 8], size=2)
-
-
-def move(row):
-    without_zeroes = filter(bool, row)
-    return right_pad(merge(without_zeroes))
-
-
-def merge(row):
-    result = []
-    digit_stack = list(reversed(row))
-    while digit_stack:
-        if len(digit_stack) == 1:
-            result.append(digit_stack.pop())
-            break
-        a = digit_stack.pop()
-        b = digit_stack.pop()
-        if a == b:
-            result.append(a+b)
-        else:
-            result.append(a)
-            digit_stack.append(b)
-    return result
-
-
-def right_pad(input_list, size=4):
-    result = input_list[:]
-    extension = [0,] * (size - len(result))
-    result.extend(extension)
-    return result
